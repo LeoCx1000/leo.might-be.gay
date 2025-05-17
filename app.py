48395602f04584443fa71d4d80839ef3f0d6ff20
Leonardo Cavenago <leonardocavenago05@gmail.com>
2025-05-17 02:55:15 +0200

    Add badge geneartor

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+import urllib.parse
 import aiohttp
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Literal
 from contextlib import asynccontextmanager
-from litestar import get, Request, Response
+from litestar import get, Request, Response, MediaType
 from litestar.response import Redirect
 from routes.files import render_code_block
 
@@ -21,28 +22,77 @@ else:
 
     _Litestar = Litestar
 
-
-async def request_obsession(app: Litestar | _Litestar, username: str):
-    params = {
-        "method": "user.getinfo",
-        "api_key": LASTFM_API_KEY,
-        "user": username,
-        "format": "json",
-    }
-    async with app.state.session.get(LASTFM_API_URL, params=params) as resp:
-        return await resp.json()
+OPEN_URL = "https://open.spotify.com/track/{0}"
 
 
-@get("/last.fm-favourite.png")
-async def last_fm_favourite() -> Redirect:
+async def request_obsession(app: Litestar | _Litestar, user_id: int):
+    async with app.state.session.get(
+        f"http://localhost:5716/obsession/{user_id}"
+    ) as resp:
+        return resp.status, await resp.json()
+
+
+async def request_playing(app: Litestar | _Litestar, user_id: int):
+    async with app.state.session.get(
+        f"http://localhost:5716/spotify/{user_id}"
+    ) as resp:
+        return resp.status, await resp.json()
+
+
+def badge_quote(string: str) -> str:
+    return urllib.parse.quote(string).replace("-", "--").replace("_", "__")
+
+
+@get("/obsession/{user_id:int}.png")
+async def last_fm_favourite(request: Request, user_id: int) -> Redirect:
+    status, obsession = await request_obsession(request.app, user_id)
+
+    if status == 404:
+        return Redirect(
+            "https://img.shields.io/badge/No obsession set.-252525?style=flat&logo=spotify"
+        )
+    track_name = obsession["title"]
+    artist = obsession["title"]
     return Redirect(
-        "https://img.shields.io/badge/Current Favourite: ないものねだり by KANA--BOON × もっさ-252525?style=flat&logo=spotify"
+        f"https://img.shields.io/badge/Current_Favourite: {badge_quote(track_name)} by {badge_quote(artist)}-252525?style=flat&logo=spotify"
     )
 
 
-@get("/last.fm-favourite")
-async def redirect_to_song(request: Request) -> dict:
-    return await request_obsession(request.app, "LeoCx1000")
+@get("/obsession/{user_id:int}")
+async def redirect_to_song(request: Request, user_id: int) -> Response:
+    status, obsession = await request_obsession(request.app, user_id)
+    if status == 404:
+        return Response(
+            'Obsession not set. Please join our discord server and use the "/obsession set" command. https://discord.gg/TdRfGKg8Wh',
+            media_type=MediaType.TEXT,
+        )
+    return Redirect(OPEN_URL.format(obsession["track_id"]))
+
+
+@get("/listening/{user_id:int}.png")
+async def currently_playing(request: Request, user_id: int) -> Redirect:
+    status, obsession = await request_playing(request.app, user_id)
+
+    if status == 404:
+        return Redirect(
+            "https://img.shields.io/badge/Not listening to anything.-252525?style=flat&logo=spotify"
+        )
+    track_name = obsession["title"]
+    artist = obsession["title"]
+    return Redirect(
+        f"https://img.shields.io/badge/Listening to {badge_quote(track_name)} by {badge_quote(artist)}-252525?style=flat&logo=spotify"
+    )
+
+
+@get("/listening/{user_id:int}")
+async def redirect_to_playing(request: Request, user_id: int) -> Response:
+    status, obsession = await request_playing(request.app, user_id)
+    if status == 404:
+        return Response(
+            "Obsession not set. Please join our discord server so our bot can see you. https://discord.gg/TdRfGKg8Wh",
+            media_type=MediaType.TEXT,
+        )
+    return Redirect(OPEN_URL.format(obsession["track_id"]))
 
 
 @asynccontextmanager

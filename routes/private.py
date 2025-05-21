d26fe0269dc095fea655cc88a9a828e674b47653
Leonardo Cavenago <leonardocavenago05@gmail.com>
2025-05-21 09:12:47 +0200

    add reboot route

---
+++ routes/private.py
@@ -0,0 +1,34 @@
+import hashlib
+import hmac
+import os
+
+from litestar import Request, Router, post
+from litestar.exceptions import HTTPException
+
+API_SECRET = os.getenv("GH_KEY", "")
+
+
+async def verify_signature(request: Request):
+    signature = (
+        "sha256="
+        + hmac.new(
+            bytes(API_SECRET, "utf-8"),
+            msg=await request.body(),
+            digestmod=hashlib.sha256,
+        ).hexdigest()
+    )
+    return hmac.compare_digest(signature, request.headers["X-Hub-Signature-256"])
+
+
+@post("/reboot", status_code=200, no_auth=True)
+async def github_webhook(request: Request) -> dict[str, str]:
+    """The GitHub webhook URL. Runs every time a push is made to the repo."""
+    if not await verify_signature(request):
+        raise HTTPException(status_code=401, detail="Invalid secret.")
+    os.system(
+        f"cd {os.getcwd()!r} && git pull && sudo systemctl restart might-be-gay.service"
+    )
+    return {"status": "ok"}
+
+
+router = Router("/private", route_handlers=[github_webhook])

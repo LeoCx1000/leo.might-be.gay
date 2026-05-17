4598dd180254787cc36e803518441365ab113865
Leonardo Cavenago <leonardocavenago05@gmail.com>
2026-05-17 14:17:41 +0200

    Add simple MD renderer

--- routes/files.py
+++ routes/files.py
@@ -1,31 +1,35 @@
 import pathlib
 import aiohttp
 
-from litestar import MediaType, Response, get, Router, Request
+from litestar import MediaType, get, Router, Request
 from litestar.exceptions import HTTPException
 from litestar.response import Template
-from litestar.controller import Controller
 import os
+from .frontend import render_markdown
 
 BASE_PATH = pathlib.Path("/www/files")
 
 
-@get("{filename:path}", media_type=MediaType.HTML)
-async def render_code_block(request: Request, filename: str) -> Template:
+def get_file(filename: str):
     file = BASE_PATH / filename.removeprefix("/")
 
     common = pathlib.Path(os.path.commonprefix([BASE_PATH, file.resolve()]))
 
     if common != BASE_PATH or not file.exists() or not file.is_file():
-        raise HTTPException(detail="File does not exist.", status_code=404)
+        raise HTTPException(detail="File does not exist", status_code=404)
     try:
-        file.read_text()
-    except:
-        raise HTTPException(detail="Failed displaying file.", status_code=404)
+        return (file, file.read_text())
+    except Exception as e:
+        raise HTTPException(detail=f"Failed displaying file. {e}", status_code=404)
+
+
+@get("code/{filename:path}", media_type=MediaType.HTML)
+async def render_code_block(request: Request, filename: str) -> Template:
+    file, _ = get_file(filename)
 
     session: aiohttp.ClientSession = request.app.state.session
     async with session.get(
-        f"http://127.0.0.1:39389/hl",
+        "http://127.0.0.1:39389/hl",
         params={
             "lang": filename.split(".")[-1],
             "theme": "github-dark",
@@ -37,4 +41,17 @@ async def render_code_block(request: Request, filename: str) -> Template:
     return Template("code.html", context={"filename": filename, "html": html})
 
 
-router = Router("code", route_handlers=[render_code_block])
+@get("md/{filename:path}")
+async def render_md_file(filename: str) -> Template:
+    print("file")
+    if not filename.endswith("md"):
+        raise HTTPException("Can only render MakDown files.")
+    _, contents = get_file(filename)
+
+    return Template(
+        "markdown.html",
+        context={"content": render_markdown(contents), "title": filename},
+    )
+
+
+router = Router("", route_handlers=[render_code_block, render_md_file])
